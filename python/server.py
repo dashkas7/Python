cf7a93239b9a77989e2b87c190884c558f6d0051
Dasha <sivcukdasa@gmail.com>
2025-09-21 22:36:55 +0300

    Первый коммит

--- python/server.py
+++ python/server.py
@@ -4,3 +4,7 @@ import socket
 #print(HOST)
 
 HOST=('127.0.0.1',7777)
+# SOCK_DGRAM - UDP, SOCK_STREAM - TCP, AF_INET - ip v4
+# sock = socket.socket(socket.AF_INET,socket.SOSCK_STREM)
+
+print(dir(socket))
