506e1e41cbb8b307224e35a0074739919338a7e7
Dasha <sivcukdasa@gmail.com>
2025-09-23 18:48:33 +0300

    обновлен

--- lesson2/server_http.py
+++ lesson2/server_http.py
@@ -1,15 +1,61 @@
+# import socket
+
+
+# def send_file(file_name, conn):
+#     try:
+#         with open(file_name.lstrip('/'), 'rb') as f:
+#             print(f"send file {file_name}") 
+#             conn.send(OK)
+#             conn.send(HEADERS)
+#             conn.send(f.read())
+#     except IOError:
+#         print(f"file not found") 
+#         conn.send(ERR_404)
+
+# sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+# sock.bind(('127.0.0.1', 7777))
+# sock.listen()
+
+# print('--start--')
+
+# OK = b'HTTP/1.1 200 OK\n'
+# HEADERS = b'Host: some.ru\nHost1: some1.ru\n\n'
+# ERR_404 = b'HTTP/1.1 404 Not Found\n\n'
+
+# while True:
+#     print('--listen--')
+#     conn, addr= sock.accept()
+#     #print('Подключился клиент:', addr)
+#     print(addr)
+#     print(conn)
+
+
+#     data = conn.recv(1024).decode("utf-8")
+#     print ('Получено:', data)
+#     # # conn.send(OK)
+#     # # conn.send(HEADERS)
+#     #conn.send(b'HELLO PYTHON:)')
+#     # html = "<H1> Hello Python!!! </H1> <p> параграф \n 123 </p>"
+#     # conn.send(html.encode("utf-8"))
+#     send_file(r"lesson2\\1.html", conn)
+    
+# send_file(file_name, conn)
+# conn.close()
+
+
+
 import socket
 
-def send_file(file_name, conn):
+def send_file(file_name, conn, content_type="text/html"):
     try:
         with open(file_name.lstrip('/'), 'rb') as f:
             print(f"send file {file_name}") 
-            conn.send(OK)
-            conn.send(HEADERS)
+            header = f"HTTP/1.1 200 OK\nContent-Type: {content_type}; charset=utf-8\n\n"
+            conn.send(header.encode("utf-8"))
             conn.send(f.read())
     except IOError:
-        print(f"file not found") 
-        conn.send(ERR_404)
+        print(f"file not found: {file_name}") 
+        conn.send(b"HTTP/1.1 404 Not Found\n\n<h1>404 Not Found</h1>")
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 sock.bind(('127.0.0.1', 7777))
@@ -17,33 +63,24 @@ sock.listen()
 
 print('--start--')
 
-OK = b'HTTP/1.1 200 OK\n'
-HEADERS = b'Host: some.ru\nHost1: some1.ru\n\n'
-ERR_404 = b'HTTP/1.1 404 Not Found\n\n'
-
 while True:
     print('--listen--')
-    conn, addr= sock.accept()
-    #print('Подключился клиент:', addr)
-    print(addr)
-    print(conn)
-
+    conn, addr = sock.accept()
 
     data = conn.recv(1024).decode("utf-8")
-    print ('Получено:', data)
-    # # conn.send(OK)
-    # # conn.send(HEADERS)
-    # # #conn.send(b'HELLO PYTHON:)')
-    # # html = "<H1> Hello Python!!! </H1> <p> параграф \n 123 </p>"
-    # #conn.send(html.encode("utf-8"))
-    # send_file(r"1.html", conn)
-    
-    try:
-        file_name = data.split('\n')[0].split(' ')[1]
-        if file_name == '/':
-            file_name = '/1.html'  # по умолчанию
-    except IndexError:
-        file_name = '/1.html'
+    print('Получено:', data)
 
-send_file(file_name, conn)
-conn.close()
+    # первая строка запроса: GET /style.css HTTP/1.1
+    try:
+        request = data.split(" ")[1]
+    except IndexError:
+        request = "/"
+
+    if request == "/" or request == "/1.html":
+        send_file("lesson2/1.html", conn, "text/html")
+    elif request == "/1.css":
+        send_file("lesson2/1.css", conn, "text/css")
+    else:
+        conn.send(b"HTTP/1.1 404 Not Found\n\n<h1>404 Not Found</h1>")
+
+    conn.close()
