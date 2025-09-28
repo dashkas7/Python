b46e7c1d8abde6745afc8b43c6fba3b24178aa2a
Dasha <sivcukdasa@gmail.com>
2025-09-29 02:17:44 +0300

    обновлен

--- lesson3_flask/main.py
+++ lesson3_flask/main.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
@@ -7,4 +7,34 @@ app = Flask(__name__)
     # view
     # controller
     
-@app.
+#app = Flask(__name__, static_folder=os.path.join(BASE_DIR, 'static'), template_folder=os.path.join(BASE_DIR, 'templates'))
+users = [ 'user1', 'user2', 'user3', 'user4', 'user5' ]
+
+@app.route("/")
+def index():
+    return render_template('1.html', admin=True, q=22222)
+
+@app.route("/message/<login>/<mes>/")
+def message(login,mes):
+    return render_template('mes.html',user=login, mes = mes)
+    
+@app.route("/users/")
+def users_():
+    return render_template('users.html', users=users)
+
+
+@app.route("/test/<int:num>/")
+def test(num):
+    return 'test' * num
+   # return render_template('test.html')
+   
+   
+   
+@app.errorhandler(404)
+def page_not_found(error):
+    return '<h1 style="color:red; text-align:center; font-size:48px">Такой страницы не существует</h1>'
+
+
+
+
+app.run(debug=True) 
