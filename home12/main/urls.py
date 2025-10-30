dc67220c5d482cb3b1e0e6b23bea9d21806f0891
Dasha <sivcukdasa@gmail.com>
2025-10-30 18:01:20 +0300

    обновлен

--- home12/main/urls.py
+++ home12/main/urls.py
@@ -5,16 +5,19 @@ app_name = "main"
 
 urlpatterns = [
     path("", views.home, name="home"),
+    
     path("students/", views.students, name="students"),
-    path("students/<int:pk>/", views.student_detail, name="student_detail"),
     path("students/add/", views.student_create, name="student_add"),
     path("students/<int:pk>/edit/", views.student_edit, name="student_edit"),
     path("students/<int:pk>/delete/", views.student_delete, name="student_delete"),
+    path("students/<int:pk>/", views.student_detail, name="student_detail"),
+   
     path("courses/", views.courses, name="courses"),
-    path("courses/<int:pk>/", views.course_detail, name="course_detail"),
     path("courses/add/", views.course_create, name="course_add"),
     path("courses/<int:pk>/edit/", views.course_edit, name="course_edit"),
     path("courses/<int:pk>/delete/", views.course_delete, name="course_delete"),
+    path("courses/<int:pk>/", views.course_detail, name="course_detail"),
+   
     path("journal/", views.journal, name="journal"),
     path("register/", views.register, name="register"),
 ]
